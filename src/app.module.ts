077bc7799c13a239713a2bd10372dd43cf81ca4e
jayarathinavel <jayrathinavel@gmail.com>
2025-09-13 23:42:37 +0530

    Moving DB Creds to .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,31 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthMiddleware } from './auth.middleware';
 import { PublicApiModule } from './public-api/public-api.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import * as crypto from 'crypto';
+(global as any).crypto = crypto;
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest-js-practice-app',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }), // Makes env variables globally available
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
-    PublicApiModule
+    PublicApiModule,
   ],
   controllers: [AppController],
   providers: [AppService],
